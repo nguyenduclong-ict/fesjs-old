1e55780fdf8b179ad2f28053e2a49702e8603ab8
Nguyen Duc Long <nguyenduclong.ict@gmail.com>
2020-05-09 10:50:00 +0700

    update mongoose connect

--- lib/mongoose/connect.d.ts
+++ lib/mongoose/connect.d.ts
@@ -1,5 +1,5 @@
 import { ConnectionOptions } from 'mongoose';
-declare type ConnectOptions = {
+interface ConnectOptions {
     host: string;
     port: string | number;
     dbName: string;
@@ -7,9 +7,6 @@ declare type ConnectOptions = {
     pass: string;
     authDb?: string;
     config: ConnectionOptions;
-};
-export declare function connect(options: ConnectOptions): import("mongoose").Connection & {
-    then: <TResult1 = import("mongoose").Connection, TResult2 = never>(onfulfilled?: (value: import("mongoose").Connection) => TResult1 | PromiseLike<TResult1>, onrejected?: (reason: any) => TResult2 | PromiseLike<TResult2>) => Promise<TResult1 | TResult2>;
-    catch: <TResult = never>(onrejected?: (reason: any) => TResult | PromiseLike<TResult>) => Promise<import("mongoose").Connection | TResult>;
-};
+}
+export declare function connect(options: ConnectOptions): Promise<import("mongoose").Connection>;
 export {};
